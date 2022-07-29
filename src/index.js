7b8912492c0475e6228bfe9bae37107ba127ea5f
Peter Zaichyk <zaichikpetr@gmail.com>
2022-07-29 22:24:00 +0300

    feat:finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const stringN = String(Math.abs(n));
+    const l = stringN.length;
+    let newN = '';
+    for (i = l - 1; i >= 0; i--) {
+        newN = newN + stringN[i];
+    }
+    return Number(newN);
 }
